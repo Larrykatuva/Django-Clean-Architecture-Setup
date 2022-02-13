4e0e1389f556e62a00a6d2519a81c732d8893921
Larrykatuva <larry.katuva@gmail.com>
2022-02-13 10:13:47 +0300

    Test sample

--- main/tests/tests.py
+++ main/tests/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+import unittest
+import django
+import pytest
 
-# Create your tests here.
+
+@pytest.mark.django_db
+class TestWorking(unittest.TestCase):
+    def test_can_confirm_test_is_working(self):
+        self.assertEqual("Working", "Working")
